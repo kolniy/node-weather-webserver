dfde3537aa56323bdafc88e2aa5f40b6e0741cd2
kolawole olaniyi <kolaniyi3@gmail.com>
2019-05-01 22:24:53 +0100

    set up app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -10,8 +10,8 @@
 
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
-cd
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+
+    fetch(`/weather?address=${location}`).then((response) => {
      response.json().then((data) => {
 
         if(data.error) { 
@@ -19,6 +19,7 @@ cd
         } else {    
         messageOne.textContent = data.location
         messageTwo.textContent = data.forecast
+     
         }
      })
  })
